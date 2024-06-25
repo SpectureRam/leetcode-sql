b0d38d5027231850b35a42f8b66c086b3563e9cf
SAIRAM N <ramphilo@gmail.com>
2024-06-25 22:57:04 +0530

    Added Medium SQL
    Added Medium SQL

---
+++ Medium/180. Consecutive Numbers.sql
@@ -0,0 +1,49 @@
+-- 180. Consecutive Numbers
+-- https://leetcode.com/problems/consecutive-numbers/description/
+-- Table: Logs
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | num         | varchar |
+-- +-------------+---------+
+-- In SQL, id is the primary key for this table.
+-- id is an autoincrement column.
+ 
+
+-- Find all numbers that appear at least three times consecutively.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Logs table:
+-- +----+-----+
+-- | id | num |
+-- +----+-----+
+-- | 1  | 1   |
+-- | 2  | 1   |
+-- | 3  | 1   |
+-- | 4  | 2   |
+-- | 5  | 1   |
+-- | 6  | 2   |
+-- | 7  | 2   |
+-- +----+-----+
+-- Output: 
+-- +-----------------+
+-- | ConsecutiveNums |
+-- +-----------------+
+-- | 1               |
+-- +-----------------+
+-- Explanation: 1 is the only number that appears consecutively for at least three times.
+
+-- Solution : 
+
+select distinct l1.num as ConsecutiveNums from logs l1,logs l2,logs l3
+where l1.id = l2.id+1 and l1.num = l2.num and l1.id = l3.id+2 and l1.num = l3.num;  
